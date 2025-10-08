65a1d8ade843f56f75b785a39c4d394ad911a6dc
Trabmini <tooktrab@icloud.com>
2025-10-08 02:35:46 -0400

    Build Update 1
    -Migration to supa (ipv4)
    -Prisma setup
    -Env update (not pushed)
    -Just schema things :)
    
    *clerk updated as well so i had to get deps again*

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,5 @@
+const SetupPage =async () => {
+    return <div>Create a Ready Room</div>;
+}
+ 
+export default SetupPage;
---
+++ lib/initial-profile.ts
@@ -0,0 +1,19 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+
+export const initialProfile = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    return redirect("/sign-in");
+  }
+
+  const profile = await db.profile.findUnique({
+    where: {
+      userId: user.id,
+    },
+  });
+
+
+}
